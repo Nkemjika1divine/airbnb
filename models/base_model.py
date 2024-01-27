601ac58c466387d3dfcbecc891abb70105652df0
Nkemjika1divine <cndivine@gmail.com>
2024-01-27 19:21:46 +0100

    task 2

--- models/base_model.py
+++ models/base_model.py
@@ -4,10 +4,13 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            
+
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """print the name of the class, id and dctionary representation"""
@@ -18,7 +21,7 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        dict_obj = self.__cdict__.copy() # copy the dictionary
+        dict_obj = self.__dict__.copy() # copy the dictionary
         dict_obj["__class__"] = self.__class__.__name__ # copy the name out and mskes it a key
         dict_obj['created_at'] = self.created_at.isoformat() # makes created_at a key
         dict_obj['updated_at'] = self.updated_at.isoformat() # makes updated_at a key
