0a441ab3af22fc5cf92dda8bb72a959ba7c07c15
Nkemjika1divine <cndivine@gmail.com>
2024-01-27 03:37:00 -0800

    airbnb remake

--- models/base_model.py
+++ models/base_model.py
@@ -4,5 +4,21 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    id = str(uuid.uuid4())
-    created_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """print the name of the class, id and dctionary representation"""
+        return "[{}] ({}) {}".format(self.__cls__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """updates the current time at updated_at"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dict_obj = self.__cdict__.copy() # copy the dictionary
+        dict_obj["__class__"] = self.__class__.__name__ # copy the name out and mskes it a key
+        dict_obj['created_at'] = self.created_at.isoformat() # makes created_at a key
+        dict_obj['updated_at'] = self.updated_at.isoformat() # makes updated_at a key
